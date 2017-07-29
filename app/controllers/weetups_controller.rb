22961264df006a27b608970355838d6882f86c6f
wangjingru <w15673142360@gmail.com>
2017-07-29 11:41:15 +0800

    实作weetup

--- app/controllers/weetups_controller.rb
+++ app/controllers/weetups_controller.rb
@@ -2,4 +2,47 @@ class WeetupsController < ApplicationController
   def index
     @weetups = Weetup.all
   end
+
+  def show
+    @weetup = Weetup.find(params[:id])
+  end
+
+  def new
+    @weetup = Weetup.new
+  end
+
+  def create
+    @weetup = Weetup.new(weetup_params)
+    if @weetup.save
+      redirect_to weetups_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @weetup = Weetup.find(params[:id])
+  end
+
+  def update
+    @weetup = Weetup.find(params[:id])
+    if @weetup.update(weetup_params)
+      redirect_to weetups_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @weetup = Weetup.find(params[:id])
+    @weetup.destroy
+    flash[:alert] = "Meetup deleted"
+    redirect_to weetups_path
+  end
+
+  private
+
+  def weetup_params
+    params.require(:weetup).permit(:title, :description)
+  end
 end
--- app/models/weetup.rb
+++ app/models/weetup.rb
@@ -1,2 +1,3 @@
 class Weetup < ApplicationRecord
+  validates :title, presence: true
 end
